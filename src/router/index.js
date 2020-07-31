751f997c1140a375571ff8c16246a501649c7c74
luoxianhe <17681833275@163.com>
2020-07-31 15:58:59 +0800

    new2修改

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import Layout from '@/layout'
 /**
  * Note: sub-menu only appear when route children.length >= 1
  * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
- *
+ *这是new222222222222改变的
  * hidden: true                   if set true, item will not show in the sidebar(default is false)
  * alwaysShow: true               if set true, will always show the root menu
  *                                if not set alwaysShow, when item has more than one children route,
